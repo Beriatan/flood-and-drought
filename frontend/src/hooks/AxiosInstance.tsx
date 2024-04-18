315e10b70e60da74f85145d175c1dbec52c6f6fe
Beriatan <michalbrenda@outlook.com>
2024-04-18 12:11:11 +0100

    Added alternative url to react base

--- frontend/src/hooks/AxiosInstance.tsx
+++ frontend/src/hooks/AxiosInstance.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL,
+    baseURL: process.env.REACT_APP_API_BASE_URL || 'http://172.17.0.2:5000',
 })
 
 export default axiosInstance
